dd588d13264568925d9ff25b49c9157dde478acb
danh181 <danhthanh18102004@gmail.com>
2025-11-04 22:53:19 +0700

    change domain BE

--- src/services/auth.js
+++ src/services/auth.js
@@ -6,7 +6,7 @@ const BASE_URL =
   import.meta.env.VITE_API_URL ||
   (import.meta.env.DEV
     ? 'http://localhost:8080' // Khi chạy npm run dev
-    : 'https://czf23bx8-8080.asse.devtunnels.ms'); // fallback khi build mà quên set env
+    : 'https://9gjwld5d-8080.asse.devtunnels.ms'); // fallback khi build mà quên set env
 
 const API = axios.create({
 	baseURL: BASE_URL,
